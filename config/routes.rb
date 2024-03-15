bc960d15d25c2ed8706788cc90747c78dc6f9423
DustinFader <dustinfader16@gmail.com>
2024-03-15 17:15:12 -0300

    create user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,12 @@ Rails.application.routes.draw do
 
   get '/about', to: 'about#index'
 
+  get '/cool' => 'gif#cool'
+  get '/sweet' => 'gif#sweet'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
